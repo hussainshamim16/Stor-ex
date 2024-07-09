6bc209665ed0658d50ac5534a79f03b7bff0de1a
MUHAMMAD HUSSAIN SHAMIM <142653978+hussainshamim16@users.noreply.github.com>
2024-07-09 07:46:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+// // console.log("hello world");
+// fetch("https://fakestoreapi.com/products/")
+
+// .then((response)=>{return response.json()})
+
+// .then((pass)=>{
+// console.log("pass",pass)
+// addFnc(pass)
+// })
+// .catch((error)=>{
+//     console.log("error",error)
+// })
+
+// function addFnc(items){
+//     const parent = document.getElementById("parent");
+//     console.log(items[0].image)
+   
+//     // console.log("red")
+//     for (const myveriable of items) {
+//         // console.log(myveriable.category
+//         // )
+//          parent.innerHTML +=`  <div class="card" style="width: 40%; padding:20px; margin:20px;">
+//             <img src="${myveriable.image}" class="card-img-top" alt="..." style="width: 100%;">
+//             <div class="card-body">
+//               <h5 class="card-title">${myveriable.category}</h5>
+//               <p class="card-text">${myveriable.description}</p> 
+
+//               <a href="javaScript:voide(0)" class="btn btn-primary">more deteils</a>
+//             </div>`
+//     }
+// }
